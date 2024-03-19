02b79de51871a0b70e8d503e105fa64980e185eb
dhearr <89125079+dhearr@users.noreply.github.com>
2024-03-19 20:43:10 +0700

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <main className="flex bg-black min-h-screen items-center justify-center p-24">
-      <h1 className="text-3xl font-bold">WELCOME TO THE HOUSE</h1>
+      <h1 className="text-3xl font-bold">Welcome to the house yo!</h1>
     </main>
   );
 }
