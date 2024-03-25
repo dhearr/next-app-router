5f28346dfd4795eb43a117ab1436401d1d7aebc9
dhearr <89125079+dhearr@users.noreply.github.com>
2024-03-25 21:43:27 +0700

    Update page.tsx

--- src/app/(admin)/dashboard/@analytics/page.tsx
+++ src/app/(admin)/dashboard/@analytics/page.tsx
@@ -1,7 +1,7 @@
 export default function Analytics() {
   return (
     <div className="w-3/6 h-96 bg-[#0a0a0a] border border-[#444746] rounded-[12px] flex justify-center items-center">
-      <h1>Analytics Page</h1>
+      <h1>analytics page</h1>
     </div>
   );
 }
